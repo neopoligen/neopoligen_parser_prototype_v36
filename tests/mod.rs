cad8f4b5344e5b78cb0eed1a1448f141f088b547
Alan Smith <commit@alanwsmith.com>
2024-05-07 08:54:42 -0400

    test empty checklist

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,17 +1,9 @@
 use neopoligen_parser_prototype_v36::*;
 use pretty_assertions::assert_eq;
 // use rstest::rstest;
+use std::fs;
 use std::path::PathBuf;
 use walkdir::WalkDir;
-use std::fs;
-
-
-
-
-
-
-
-
 
 // #[case(
 //     "Generic Tag",
@@ -43,10 +35,15 @@ fn run_tests() {
     let dir = PathBuf::from("tests");
     get_files(&dir, vec!["txt"]).iter().for_each(|f| {
         let content = fs::read_to_string(f).unwrap();
-        let tests = content.split("################################################").collect::<Vec<&str>>();
+        let tests = content
+            .split("################################################")
+            .collect::<Vec<&str>>();
         tests.iter().for_each(|t| {
-            let parts = t.split("------------------------------------------------").map(|p| p.trim_start()).collect::<Vec<&str>>();
-            if !parts[0].starts_with("skip") && parts.len() == 3{
+            let parts = t
+                .split("------------------------------------------------")
+                .map(|p| p.trim_start())
+                .collect::<Vec<&str>>();
+            if !parts[0].starts_with("skip") && parts.len() == 3 {
                 let left = parts[2].trim().replace("\n", "").replace(" ", "");
                 let out = output(&parse(parts[1]).unwrap());
                 let right = out.trim().replace("\n", "").replace(" ", "");
@@ -59,11 +56,10 @@ fn run_tests() {
 fn get_files(dir: &PathBuf, exts: Vec<&str>) -> Vec<PathBuf> {
     WalkDir::new(dir)
         .into_iter()
-        .filter(|e| 
-            match e.as_ref().unwrap().path().extension() {
-                Some(x) => exts.contains(&x.to_str().unwrap()),
-                None => false
-    }).map(|e| e.unwrap().into_path()).collect()
+        .filter(|e| match e.as_ref().unwrap().path().extension() {
+            Some(x) => exts.contains(&x.to_str().unwrap()),
+            None => false,
+        })
+        .map(|e| e.unwrap().into_path())
+        .collect()
 }
-
-
